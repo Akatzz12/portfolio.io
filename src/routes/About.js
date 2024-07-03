19acc6024af28329a30b14d2fce1217bb013b19e
Kapil Ray <kapileshworray@gmail.com>
2024-07-03 14:59:00 +0530

    remove unused

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+
 import HeroImg2 from '../components/HeroImg2';
 import AboutContent from '../components/AboutContent';
 import PricingCard from '../components/PricingCard';
